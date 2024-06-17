95de4b12840f487f1a2b6473c8df78f4a9cbbfa8
ddochiisrich <ddochiisrich@gmail.com>
2024-06-17 14:10:53 +0900

    점심시간 수업

--- NodeClassCh03/my-app01/index02.js
+++ NodeClassCh03/my-app01/index02.js
@@ -1,9 +1,24 @@
 // 미들웨어 (Middleware)
 const express = require('express')
 const url = require('url')
+const createError = require('http-errors');
 
 const app = express()
 const port = 3000
+
+// 첫 번째 미들웨어
+app.use((req, res, next) => {
+  console.log("첫 번째 미들웨어...");
+  next();
+});
+
+// 두 번째 미들웨어 정의
+let requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+}
+
+app.use(requestTime);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,18 +28,21 @@ app.get("/main", (req, res) => {
   // localhost:3000/main?name=홍길동&age=33
   const user = url.parse(req.url, true).query;
   console.log(user);
-  res.json({name: user.name + " - 한글", age: user.age});
+  res.json({name: user.name + " - 한글", age: user.age, reqTime: req.requestTime});
 });
 
-app.get("/list", (req, res) => {
+app.use("/list", (req, res) => {
   res.setHeader("Content-type", "text/html; charset=UTF-8");
   res.end(`<h1>여기는 리스트</h1>
             <ul>
-              <li>Node.js</li>
-              <li>JavaScript</li>
-              <li>JSON</li>
+              <li>app.use("/list", (req, res) => {}) 라우팅</li>
             </ul>`)
 });
+
+// 요청 페이지가 없을 때 - 에러를 발생시키는 미들웨어
+app.use((req, res, next) => {
+  next(createError(404));
+})
 
 app.listen(port, () => {
   console.log(`Express 서버 시작 - OK - http://localhost:${port}`);
